fc289e83c9d2c78e590fb99fa6415670f871af56
Tuan Nguyen <tuan.nguyen@digitalengine.se>
2025-05-21 17:37:31 +0200

    updating repos

--- src/main/java/com/example/coday/repository/UserRepo.java
+++ src/main/java/com/example/coday/repository/UserRepo.java
@@ -3,6 +3,7 @@ package com.example.coday.repository;
 import com.example.coday.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,9 +15,12 @@ public interface UserRepo extends JpaRepository<User, Long> {
             "(LOWER(u.firstName) LIKE LOWER(CONCAT('%', :name, '%')) " +
             "OR LOWER(u.lastName) LIKE LOWER(CONCAT('%', :name, '%'))) " +
             "AND u.points >= :minPoints")
+    List<User> searchByNameAndMinPoints(@Param("name") String name, @Param("minPoints") int minPoints);
 
     List<User> findByApprovedTrue();
+
     List<User> findAllByRole(User.Role role);
+
     Optional<User> findByEmailIgnoreCase(String email);
     List<User> findByCompanyIdAndApprovedTrue(Long companyId);
 
